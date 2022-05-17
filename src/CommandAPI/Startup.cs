689f89f8638b02c01fd728e295dbcc35526aa371
Casimiro Greco <casimirogreco@gmail.com>
2022-05-17 20:36:44 +0200

    Added Automapper and DTO Class

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using AutoMapper;
 
 namespace CommandAPI
 {
@@ -34,6 +35,7 @@ namespace CommandAPI
          services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(builder.ConnectionString));
 
          services.AddControllers();
+         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
          //  services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
          services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
          var valueFromConfiguration = Configuration["rootObject:value"];
